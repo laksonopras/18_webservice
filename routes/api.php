062f3392653d9cd0b67888492b790f834cc3dc9c
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-01 16:08:55 +0700

    resetup api route, add partner detail request

--- routes/api.php
+++ routes/api.php
@@ -23,18 +23,18 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(['middleware' => 'api', 'prefix' => 'user' ], function ($router) {
+Route::group(['middleware' => 'api', 'prefix' => 'user'], function ($router) {
     Route::post('register', [UserController::class, 'register']);
     Route::post('login', [UserController::class, 'login']);
     Route::post('logout', [UserController::class, 'logout']);
     // Route::post('refresh', 'AuthController@refresh');
     Route::get('me', [UserController::class, 'show']);
     Route::get('category', [CategoryController::class, 'index']);
-    
+
     Route::post('editProfile', [UserController::class, 'update']);
     Route::get('avatar/{id}', [UserController::class, 'getAvatar']);
     Route::get('avatar', [UserController::class, 'getUserAvatar']);
-    
+
     Route::post('mitra', [PartnerController::class, 'store']);
 });
 
@@ -43,7 +43,7 @@ Route::group(['middleware' => 'api', 'prefix' => 'admin'], function ($router) {
     Route::post('login', [AdminController::class, 'login']);
     Route::post('logout', [AdminController::class, 'logout']);
     Route::get('me', [AdminController::class, 'me']);
-    
+
 
     //Customer
     Route::get('customer', [UserController::class, 'index']);
@@ -57,17 +57,19 @@ Route::group(['middleware' => 'api', 'prefix' => 'admin'], function ($router) {
     Route::post('category/', [CategoryController::class, 'store']);
     Route::put('category/{id}', [CategoryController::class, 'update']);
     Route::delete('category/{id}', [CategoryController::class, 'destroy']);
-    
+
     //Banner
     Route::get('banner', [BannerController::class, 'index']);
     Route::get('banner/{id}', [BannerController::class, 'show']);
     Route::post('banner/', [BannerController::class, 'store']);
     Route::put('banner/{id}', [BannerController::class, 'update']);
     Route::delete('banner/{id}', [BannerController::class, 'destroy']);
-    
-    
+
+
     // Route::post('refresh', 'AuthController@refresh');
     Route::get('/partner', [PartnerController::class, 'index']);
+    Route::get('/partner/{id}', [PartnerController::class, 'showDetail']);
+    Route::patch('/partner', [PartnerController::class, 'update']);
     Route::get('/partner/avatar/{id}', [PartnerController::class, 'getAvatar']);
 });
 
