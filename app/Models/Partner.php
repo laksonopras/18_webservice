2caadec2ae8e57a4ac3885b6b77884ea84d2277e
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-01 21:51:23 +0700

    add relation for partner to user

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -25,11 +25,16 @@ class Partner extends Model
         'token'
     ];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
-    public function admin(){
+    public function admin()
+    {
         return $this->belongsTo(Admin::class);
     }
-    
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
