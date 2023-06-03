9ccb7b0896d47759b4e8305831e95ea7e6211d25
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-03 22:42:14 +0700

    call transaction seeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CategorySeeder::class,
             PartnerSeeder::class,
+            TransactionSeeder::class,
         ]);
     }
 }
