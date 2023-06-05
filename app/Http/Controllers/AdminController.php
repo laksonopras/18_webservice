8e0c177bed2688dca09bcaaa4f2ccd043ff201de
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-06 03:25:04 +0700

    change return for get admin login info

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,19 +12,21 @@ use Illuminate\Validation\Rules;
 
 class AdminController extends Controller
 {
-    public function _construct() {
+    public function _construct()
+    {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $rules = array(
             'username' => ['required'],
             'email' => ['required', 'email', 'unique:admins'],
             'password' => ['required', Rules\Password::defaults()]
         );
-        
+
         $validate = Validator::make($request->all(), $rules);
-        if($validate->fails()){
+        if ($validate->fails()) {
             return response()->json([
                 'status' => false,
                 'message' => $validate->messages()->first()
@@ -43,13 +45,14 @@ class AdminController extends Controller
         }
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $rules = array(
             'email' => ['required', 'email'],
             'password' => ['required', Rules\Password::defaults()]
         );
         $validate = Validator::make($request->all(), $rules);
-        if($validate->fails()){
+        if ($validate->fails()) {
             return response()->json([
                 'status' => false,
                 'message' => $validate->messages()->first()
@@ -63,8 +66,8 @@ class AdminController extends Controller
                 ]);
             }
             $admin = Admin::where('email', $request->email)->first();
-            $admin->token = $token; 
-            $admin->save();      
+            $admin->token = $token;
+            $admin->save();
             return response()->json([
                 'status' => true,
                 'message' => 'successfully login',
@@ -74,10 +77,11 @@ class AdminController extends Controller
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         $admin = Admin::where('email', auth('admin')->user()->email)->first();
-        $admin->token = null; 
-        $admin->save(); 
+        $admin->token = null;
+        $admin->save();
         auth('admin')->logout();
         return response()->json([
             'status' => true,
@@ -85,7 +89,8 @@ class AdminController extends Controller
         ]);
     }
 
-    public function index() {
+    public function index()
+    {
         $admin = Admin::all();
         return response()->json([
             'status' => true,
@@ -97,10 +102,16 @@ class AdminController extends Controller
     public function getByToken()
     {
         $admin = Admin::find(auth('admin')->user()->id);
+
         return response()->json([
             'status' => true,
             'message' => 'Show all data',
-            'admin' => $admin
+            'admin' => [
+                'id' => $admin->id,
+                'username' => $admin->username,
+                'email' => $admin->email,
+                'password' => $admin->password,
+            ]
         ]);
     }
 
@@ -117,11 +128,17 @@ class AdminController extends Controller
     public function update(Request $request)
     {
         $admin = Admin::find(auth('admin')->user()->id);
-        if($request->username){
+        if ($request->email) {
+            $admin->email = $request->email;
+        }
+        if ($request->username) {
             $admin->username = $request->username;
         }
-        if($request->file('avatar')){
-            if($admin->avatar && Storage::exists($admin->avatar)){
+        if ($request->password) {
+            $admin->password = bcrypt($request->password);
+        }
+        if ($request->file('avatar')) {
+            if ($admin->avatar && Storage::exists($admin->avatar)) {
                 Storage::delete($admin->avatar);
             }
             $admin->avatar = Storage::putFile('admin', $request->file('avatar'));
